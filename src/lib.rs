d155aa10855cfd617d45126f1b99692541c3de3d
Richard Puckett <richard@open-archive.org>
2024-09-05 17:50:48 -0700

    Extracted common code out of the Android conditional block

--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,87 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::get;
+use actix_web::post;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Serialize, Deserialize)]
 struct Message {
     content: String,
 }
 
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello from Rust server on Android using Unix socket!")
-}
+mod server {
+    use super::*;
 
-async fn echo(message: web::Json<Message>) -> impl Responder {
-    HttpResponse::Ok().json(message.0)
+    #[get("/")]
+    async fn hello() -> impl Responder {
+        HttpResponse::Ok().body("Hello from Rust server!")
+    }
+
+    #[post("/echo")]
+    async fn echo(message: web::Json<Value>) -> impl Responder {
+        HttpResponse::Ok().json(message.0)
+    }
+
+    #[get("/status")]
+    async fn status() -> impl Responder {
+        HttpResponse::Ok().json(serde_json::json!({
+            "status": "running",
+            "version": env!("CARGO_PKG_VERSION")
+        }))
+    }
+
+    pub async fn start_server(socket_path: &str) -> std::io::Result<()> {
+        HttpServer::new(|| {
+            App::new()
+            .service(hello)
+            .service(echo)
+            .service(status)
+        })
+        .bind_uds(&socket_path).unwrap()
+        .run()
+        .await
+    }
 }
 
 #[cfg(target_os = "android")]
 mod android {
+    use super::*;
     use jni::JNIEnv;
     use jni::objects::{JClass, JString};
     use jni::sys::jstring;
-    use actix_web::{web, App, HttpServer};
-    use std::sync::Arc;
-    use tokio::sync::Mutex;
     use std::fs;
 
     #[no_mangle]
-    pub extern "system" fn Java_net_opendasharchive_openarchive_features_main_RustBridge_startServer(env: JNIEnv, _: JClass, socket_path: JString) -> jstring {
-        
+    pub extern "system" fn Java_net_opendasharchive_openarchive_features_main_RustyServerManager_startServer(env: JNIEnv, _: JClass, socket_path: JString) -> jstring {
         let socket_path: String = unsafe {
-            env.get_string_unchecked(&socket_path).expect("Couldn't get socket path string").into()
+            env
+                .get_string_unchecked(&socket_path)
+                .expect("Couldn't get socket path string")
+                .into()
         };
 
-        let runtime = tokio::runtime::Runtime::new().unwrap();
-        
-        runtime.block_on(async {
-            // Remove the socket file if it already exists
-            if fs::metadata(&socket_path).is_ok() {
-                fs::remove_file(&socket_path).unwrap();
-            }
-    
-            let server = HttpServer::new(|| {
-                App::new()
-                    .route("/echo", web::post().to(echo))
-            })
-            .bind_uds(&socket_path).unwrap()
-            .run();
-    
-            let server = Arc::new(Mutex::new(Some(server)));
-            let server_clone = server.clone();
-    
-            tokio::spawn(async move {
-                if let Some(server) = server_clone.lock().await.take() {
-                    server.await.unwrap();
-                }
+        let thread_socket_path = socket_path.clone();
+
+        if fs::metadata(&socket_path).is_ok() {
+            fs::remove_file(&thread_socket_path).unwrap();
+        }
+
+        std::thread::spawn(move || {
+            let runtime = tokio::runtime::Runtime::new().unwrap();
+            runtime.block_on(async {
+                server::start_server(&thread_socket_path).await.unwrap();
             });
         });
     
         let output = env
             .new_string(format!("Server started on Unix socket: {}", socket_path))
             .expect("Couldn't create java string!");
-        output.into_raw()
-    }
 
-    async fn echo(message: web::Json<serde_json::Value>) -> web::Json<serde_json::Value> {
-        message
-    }
-
-    #[no_mangle]
-    pub extern "system" fn Java_net_opendasharchive_openarchive_features_main_RustBridge_echo<'local>(env: JNIEnv, _: JClass, input: JString) -> jstring {
-        let input: String = unsafe {
-            env.get_string_unchecked(&input).expect("Couldn't get java string!").into()
-        };
-        let output = format!("Echo: {}", input);
-        let output = env.new_string(output).expect("Couldn't create java string!");
         output.into_raw()
     }
 }
+
+#[cfg(feature = "ios")]
+mod ios { 
+    // Placeholder
+ }
